02c8d712f451c7eb816ffb783c092fa3b34cc4a1
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-06-30 17:35:40 +0000

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,24 @@
 # Write your code here
+       require 'open-uri'
+    require 'net/http'
+class GetRequester
+
+  attr_accessor :url
+  def initialize(url)
+    @url = url
+   
+  end
+
+  def get_response_body
+  # binding.pry 
+   uri = URI.parse(@url)
+   uri.open.string
+  end
+
+  def parse_json
+       uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    JSON.parse(response.body)
+  end
+
+end 
